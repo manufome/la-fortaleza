0310d23376ff9ddda2382fb0ac1b11779b785824
manufome <152039260+manufome@users.noreply.github.com>
2023-12-05 11:13:48 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,58 @@
-let loadMoreBtn = document.querySelectorAll('.load-more');
+let loadMoreBtn = document.querySelectorAll(".load-more");
 let currentItem = 8;
 
-
-
 loadMoreBtn.onclick = () => {
-    console.log('hola')
-    let boxes = [document.querySelectorAll('.box-container .box')];
-    for(var i = currentItem; i < currentItem + 4; i++ ){
-        boxes[i].style.display = 'inline-block';
+    console.log("hola");
+    let boxes = [document.querySelectorAll(".box-container .box")];
+    for (var i = currentItem; i < currentItem + 4; i++) {
+        boxes[i].style.display = "inline-block";
     }
     currentItem += 4;
-    if(currentItem >= boxes.length){
-        loadMoreBtn.style.display = 'none'
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = "none";
     }
-}
-// En esta parte vasmos a colocar las funciones del carrito
-
-const carrito = document.getElementById('carrito');
-const elementos1 = document.getElementById('lista-1');
-const elementos2 = document.getElementById('lista-2');
-const elementos3 = document.getElementById('lista-3');
-const elementos4 = document.getElementById('lista-4');
-const lista = document.querySelector('#lista-carrito tbody');
-const vaciarcarritoBtn = document.getElementById('vaciar-carrito');
+};
+const carrito = document.getElementById("carrito");
+const lista = document.querySelector("#lista-carrito tbody");
+const vaciarcarritoBtn = document.getElementById("vaciar-carrito");
 
 cargarEventListeners();
-function cargarEventListeners() { 
-elementos1.addEventListener('click', comprarElemento);
-elementos2.addEventListener('click', comprarElemento);
-elementos3.addEventListener('click', comprarElemento);
-elementos4.addEventListener('click', comprarElemento);
-carrito.addEventListener('click', eliminarElemento);
-vaciarcarritoBtn.addEventListener('click', vaciarCarrito);
+
+function cargarEventListeners() {
+    document.body.addEventListener("click", function (e) {
+        if (e.target.classList.contains("agregar-carrito")) {
+            e.preventDefault();
+            const elemento = e.target.closest(".box");
+            leerDatosElemento(elemento);
+        }
+        if (e.target.classList.contains("borrar")) {
+            e.preventDefault();
+            eliminarElemento(e);
+        }
+    });
+
+    vaciarcarritoBtn.addEventListener("click", vaciarCarrito);
 }
-function comprarElemento (e) {
+
+function comprarElemento(e) {
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
+    if (e.target.classList.contains("agregar-carrito")) {
         const elemento = e.target.parentElement.parentElement;
         leerDatosElemento(elemento);
     }
 }
-function leerDatosElemento(elemento){
+function leerDatosElemento(elemento) {
     const infoElemento = {
-        imagen: elemento.querySelector('img').src,
-        titulo: elemento.querySelector('h3').textContent,
-        precio: elemento.querySelector('.precio').textContent,
-        id: elemento.querySelector('a').getAttribute('data-id')
-    }
-    insertarcarrito(infoElemento); 
+        imagen: elemento.querySelector("img").src,
+        titulo: elemento.querySelector("h3").textContent,
+        precio: elemento.querySelector(".precio").textContent,
+        id: elemento.querySelector("a").getAttribute("data-id"),
+    };
+    insertarcarrito(infoElemento);
 }
-function insertarcarrito(elemento){
-    const row = document.createElement('tr');
-    row.innerHTML= `
+function insertarcarrito(elemento) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
     <td>
     <img src="${elemento.imagen}" width=100 height=150px >
     </td>
@@ -65,22 +66,21 @@ function insertarcarrito(elemento){
     <a href="#" class="borrar" data-id="${elemento.id}">x</a>
     <td>
     `;
-    lista.appendChild(row); 
+    lista.appendChild(row);
 }
 
-function eliminarElemento(e){
+function eliminarElemento(e) {
     e.preventDefault();
     elementoId;
-if(e.target.classList.contains('borrar')){
-    e.target.parentElement.parentElement.remove();
-    elemento = e.target.parentElement.parentElement;
-    elementoId = elemento.querySelector('a').getAttribute('data-id');
-
+    if (e.target.classList.contains("borrar")) {
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector("a").getAttribute("data-id");
+    }
 }
-}
-function vaciarCarrito(){
-    while(lista.firstChild){
+function vaciarCarrito() {
+    while (lista.firstChild) {
         lista.removeChild(lista.lastChild);
     }
-    return false; 
+    return false;
 }
